038433c8f232ac8957de0f07818214a32d52305c
Todd <twitzel@gmail.com>
2013-12-27 11:02:45 -0700

    started powered node

---
+++ gatherer_powerednode/Visual Micro/.gatherer_powerednode.vsarduino.h
@@ -0,0 +1,21 @@
+/* 
+	Editor: http://www.visualmicro.com
+	        arduino debugger, visual micro +, free forum and wiki
+	
+	Hardware: Arduino Uno, Platform=avr, Package=arduino
+*/
+
+#define __AVR_ATmega328P__
+#define ARDUINO 101
+#define ARDUINO_MAIN
+#define F_CPU 16000000L
+#define __AVR__
+#define __cplusplus
+extern "C" void __cxa_pure_virtual() {;}
+
+//
+//
+
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
+#include "C:\level451\gatherer\gatherer_powerednode\gatherer_powerednode.ino"
---
+++ gatherer_powerednode/gatherer_powerednode.ino
@@ -0,0 +1,9 @@
+void setup(){
+Serial.begin(115200);
+Serial.print("test");
+}
+void loop(){
+Serial.print("test - loop");
+delay(1000);
+
+}
