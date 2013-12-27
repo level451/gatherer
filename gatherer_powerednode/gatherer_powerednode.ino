024473f29cd5d0d37ec6a2f74d4d57633ce2be87
Todd <twitzel@gmail.com>
2013-12-27 14:34:18 -0700

    With Library

--- gatherer_powerednode/gatherer_powerednode.ino
+++ gatherer_powerednode/gatherer_powerednode.ino
@@ -1,9 +1,16 @@
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(6,15, NEO_GRB + NEO_KHZ800);
 void setup(){
 Serial.begin(115200);
 Serial.print("test");
+ strip.begin();
+ for (int i=0; i < 6; i++) {
+strip.setPixelColor(i,strip.Color(1,1,1));
+ }
+  strip.show();
 }
 void loop(){
 Serial.print("test - loop");
 delay(1000);
-
 }
